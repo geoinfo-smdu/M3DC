008004d4e1718bc3cdc1ff4d608dbb227729b894
Fernando Gomes <feromes@gmail.com>
2019-10-02 17:08:55 -0300

    Adicionando arquivos LAZ da cidade de São Paulo

---
+++ converted_files/pointclouds/m3dc_potree/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 110500275,
+    "boundingBox": {
+        "lx": 329316.21,
+        "ly": 7394384.100000001,
+        "lz": 0.0,
+        "ux": 332544.10000000006,
+        "uy": 7397611.99,
+        "uz": 3227.890000000014
+    },
+    "tightBoundingBox": {
+        "lx": 329316.21,
+        "ly": 7394384.100000001,
+        "lz": 0.0,
+        "ux": 332544.10000000006,
+        "uy": 7397284.16,
+        "uz": 912.74
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "CLASSIFICATION"
+    ],
+    "spacing": 27.954347610473634,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
